fac3e6567bb4a988ef9ddda2754d16d89c28fb62
Abdulaziz Ali <guuree1982@gmail.com>
2022-06-11 11:47:32 -0400

    defined function and added form tag

--- script.js
+++ script.js
@@ -3,28 +3,19 @@ let multiplicant = Math.floor((Math.random() * 100) + 1);
 
 let answer = multiplier * multiplicant
 
-let question = prompt("what is the product of  : " + multiplier  +  " X "  +  multiplicant + " ? ");
-let student_answer = prompt("enter Answer here: " )
+// let question = window.prompt("what is the product of  : " + multiplier  +  " X "  +  multiplicant + " ? ");
+// let student_answer = prompt("enter Answer here: " )
+// // document.body.append(multiplier)
+// // document.body.append(multiplicant)
+// // document.body.append(question)
+// //document.body.append(student_answer)
 
-document.body.append(multiplier)
-document.body.append(multiplicant)
+// document.getElementById("btn_Start")
+// btn_Start.addEventListener("mouseover", mouseHoverFunction);
+function time_table_practice_or_quize(){   
+    let askquestions = document.querySelector("#Ask_question + label");
 
-// // ckeck the answer:
 
-// if(student_answer === answer){
-
-//     prompt("you are correct,congratulations")
-// }else {prompt("not right, the answer is:"  + answer  )}
-
-let questions = document.addEventListener("click", FiFunction);
-
-    console.log(answer)
-
-    function FiFunction() {
-      document.getElementById("question").innerHTML = "questions:";
-    //   let question = prompt("what is the product of  : " + multiplier  +  " X "  +  multiplicant + " ? ");
-    //   let student_answer = prompt("enter Answer here: " )
-    // 
 }
-    FiFunction();
 
+time_table_practice_or_quize();
