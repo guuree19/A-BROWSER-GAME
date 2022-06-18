0f42680d49263eab4ca963d22da3e81122bb834b
Abdulaziz Ali <guuree1982@gmail.com>
2022-06-17 22:55:10 -0400

    resized two start and next buttons,and repositioned score card ,next button, and start button

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 
-
-
-
-
-
-
 let quizwraper = document.getElementById("quiz-wraper")
 let question = document.getElementById("question")
 let questiontwo = document.getElementById("parent-question");
@@ -19,48 +13,44 @@ askquestion()
 
 function askquestion(){
 
-let multiplier = Math.floor((Math.random() * 10) + 1);
-let multiplicant = Math.floor((Math.random() * 10) + 1);
-let storeanswer = multiplier * multiplicant
-buttonstart.style.background = "blue"
-questiontwo.style.display = "none"
-buttonstart.style.display = "block"
-question.textContent = `${multiplier} X ${multiplicant}  ?`
-let askquestions = document.getElementById("user-answer");
-useranswer.textContent = 0 
-console.log(useranswer.textContent)
+    let multiplier = Math.floor((Math.random() * 10) + 1);
+    let multiplicant = Math.floor((Math.random() * 10) + 1);
+    let storeanswer = multiplier * multiplicant
+    buttonstart.style.background = "blue"
+    questiontwo.style.display = "none"
+    buttonstart.style.display = "block"
+    question.textContent = `${multiplier} X ${multiplicant}  ?`
+    let askquestions = document.getElementById("user-answer");
+    useranswer.textContent = 0 
+    console.log(useranswer.textContent)
 
-buttonnext.addEventListener('click', ()=> {
-    
+    buttonnext.addEventListener('click', ()=> {
+
     count += 5;
     scorecard.textContent = count;
 
-})
-
-
+    })
 
 }
 
 
 function nextquestion(){
      
-askquestion()
+    askquestion()
 }
+
+
 function time_table_practice_or_quize(){ 
     buttonnext.addEventListener('click', nextquestion)
     
-
-
-
     time_table_practice_checking();
     
 }
 
-               time_table_practice_or_quize();
+time_table_practice_or_quize();
 
 
-
-     // checking function
+// checking function
     
     function time_table_practice_checking(){
         buttonstart.addEventListener('click', () =>{
@@ -69,21 +59,10 @@ function time_table_practice_or_quize(){
             
             buttonnext.addEventListener('click', () =>{
             questiontwo.style.display = "block"
-
-
-
-
             })
 
-            
-                
-
-
-
-            
         })
-
     }
 
-    $('body').css("background-color", "black");
+    $('body').css("background-color", "white");
      
